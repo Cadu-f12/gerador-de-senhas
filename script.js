f1909419e36e5c92f63ddf0c1267427ff7ad9b5a
CarlosWindows <cadudailha68@gmail.com>
2026-02-11 12:51:01 -0300

    feat: Add a input number which defines a password length

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function renderPassword() {
+function renderPassword(lengthPassword) {
     const paragraphElem = document.querySelector('p');
-    const password = loadPassword();
+    const password = loadPassword(lengthPassword);
 
     paragraphElem.innerHTML = password;
 }
-function loadPassword() {
+function loadPassword(lengthPassword) {
+    if (lengthPassword < 0 || lengthPassword > 16 ) {
+        return 'Number error'
+    }
+    
     const randomNum = Math.random();
     const numString = String(randomNum);
 
     // Up to 16 digits
 
     let password = '';
-    for (i = 2; i < 10; i++) {
+    for (i = 2; i <= (lengthPassword + 1); i++) {
         password += numString[i];
     }
 
